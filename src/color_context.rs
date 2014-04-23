ebb14a7c03a6afbe3c271450851b7f6b4f51a077
Sven Nilsen <bvssvni@gmail.com>
2014-04-24 00:04:12 +0200

    Moved ColorContext to own submodule

--- src/color_context.rs
+++ src/color_context.rs
@@ -1,47 +1,7 @@
-#![crate_id = "graphics"]
-#![deny(missing_doc)]
 
-//! Attempt of creating a cheap drawing context.
-
-pub use context::Context;
-pub use backend::BackEnd;
-pub use transform2d::Transform2d;
-pub use rectangle_context::RectangleContext;
-pub use rectangle_color_context::RectangleColorContext;
-
+use {Field, Borrowed, Value};
 use vecmath::{rotate_radians, multiply, translate, scale, shear};
-
-mod context;
-mod backend;
-mod transform2d;
-mod rectangle_context;
-mod rectangle_color_context;
-pub mod vecmath;
-
-pub type Matrix2d = [f64, ..6];
-pub type Color = [f64, ..4];
-pub type Rectangle = [f64, ..4];
-
-/// A structure that might contain a value or a borrowed value.
-/// This is to used as building block to create data structure
-/// that is partially based on an existing structure.
-pub enum Field<'a, T> {
-    /// Contains a value.
-    Value(T),
-    /// Contains a borrowed pointer.
-    Borrowed(&'a T),
-}
-
-impl<'a, T> Field<'a, T> {
-    /// Gets a read only value.
-    #[inline(always)]
-    pub fn get(&'a self) -> &'a T {
-        match *self {
-            Value(ref val) => val,
-            Borrowed(rval) => rval,
-        }
-    }
-}
+use {Transform2d, Matrix2d, Color};
 
 /// A context with color information.
 pub struct ColorContext<'a> {
