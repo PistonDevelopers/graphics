24b64e029b806ec09635e3dc388f0eba6d64a1f8
Sven Nilsen <bvssvni@gmail.com>
2014-04-24 18:52:53 +0200

    AddEllipse trait

---
+++ src/add_ellipse.rs
@@ -0,0 +1,33 @@
+
+/// Implemented by all contexts that can add ellipse.
+pub trait AddEllipse<'a, T> {
+    /// Adds an ellipse.
+    fn ellipse(&'a self, x: f64, y: f64, w: f64, h: f64) -> T;
+
+    /// Adds an ellipse with coordinates in the center.
+    #[inline(always)]
+    fn ellipse_centered(&'a self, center_x: f64, center_y: f64, radius_width: f64, radius_height: f64) -> T {
+        self.ellipse(
+            center_x - radius_width,
+            center_y - radius_height,
+            2.0 * radius_width,
+            2.0 * radius_height
+       )
+    }
+
+    /// Adds a square with coordinates of upper left corner.
+    #[inline(always)]
+    fn circle(&'a self, x: f64, y: f64, w: f64) -> T {
+        self.ellipse(x, y, w, w)
+    }
+
+    /// Adds a square with coordinates in the center.
+    #[inline(always)]
+    fn circle_centered(&'a self, center_x: f64, center_y: f64, radius: f64) -> T {
+        self.ellipse(center_x - radius, 
+                  center_y - radius, 
+                  2.0 * radius, 
+                  2.0 * radius)
+    }
+}
+
