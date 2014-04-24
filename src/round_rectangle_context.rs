de0a8fc78dad664af3f15e78ae50cfd262b7eb5e
Sven Nilsen <bvssvni@gmail.com>
2014-04-24 18:47:38 +0200

    Implemented Transform2d for RoundRectangleContext

--- src/round_rectangle_context.rs
+++ src/round_rectangle_context.rs
@@ -1,4 +1,6 @@
-use {Field, Matrix2d, RoundRectangle};
+use {Field, Borrowed, Value, Matrix2d, RoundRectangle};
+use vecmath::{translate, rotate_radians, scale, shear, multiply};
+use {Transform2d};
 
 /// A round rectangle context.
 pub struct RoundRectangleContext<'a> {
@@ -8,4 +10,54 @@ pub struct RoundRectangleContext<'a> {
     pub transform: Field<'a, Matrix2d>,
     /// Current round rectangle.
     pub round_rect: Field<'a, RoundRectangle>,
+}
+
+impl<'a> Transform2d<'a> for RoundRectangleContext<'a> {
+    #[inline(always)]
+    fn trans(&'a self, x: f64, y: f64) -> RoundRectangleContext<'a> {
+        RoundRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let trans = translate(x, y);
+                Value(multiply(&trans, self.transform.get()))
+            },
+            round_rect: Borrowed(self.round_rect.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn rot_rad(&'a self, angle: f64) -> RoundRectangleContext<'a> {
+        RoundRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let rot = rotate_radians(angle);
+                Value(multiply(&rot, self.transform.get()))
+            },
+            round_rect: Borrowed(self.round_rect.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn scale(&'a self, sx: f64, sy: f64) -> RoundRectangleContext<'a> {
+        RoundRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let scale = scale(sx, sy);
+                Value(multiply(&scale, self.transform.get()))
+            },
+            round_rect: Borrowed(self.round_rect.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn shear(&'a self, sx: f64, sy: f64) -> RoundRectangleContext<'a> {
+        RoundRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let shear = shear(sx, sy);
+                Value(multiply(&shear, self.transform.get()))
+            },
+            round_rect: Borrowed(self.round_rect.get()),
+        }
+    }
 }
