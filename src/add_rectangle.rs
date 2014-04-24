20539a9d7def0761d9cf876853b8c60b3ba9db66
Sven Nilsen <bvssvni@gmail.com>
2014-04-24 10:27:33 +0200

    Added square method

--- src/add_rectangle.rs
+++ src/add_rectangle.rs
@@ -3,4 +3,10 @@
 pub trait AddRectangle<'a, T> {
     /// Adds a rectangle.
     fn rect(&'a self, x: f64, y: f64, w: f64, h: f64) -> T;
+
+    /// Adds a square with coordinates of upper left corner.
+    #[inline(always)]
+    fn square(&'a self, x: f64, y: f64, w: f64) -> T {
+        self.rect(x, y, w, w)
+    }
 }
