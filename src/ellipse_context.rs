0878134915663e4702bffbab5b82f9a2adfbeb45
Sven Nilsen <bvssvni@gmail.com>
2014-04-24 18:17:50 +0200

    Implemented Transform2d for EllipseContext

--- src/ellipse_context.rs
+++ src/ellipse_context.rs
@@ -1,4 +1,7 @@
 use {Field, Matrix2d, Rectangle};
+use {Transform2d};
+use {Borrowed, Value};
+use vecmath::{translate, multiply, rotate_radians, scale, shear};
 
 /// An ellipse context.
 pub struct EllipseContext<'a> {
@@ -8,5 +11,55 @@ pub struct EllipseContext<'a> {
     pub transform: Field<'a, Matrix2d>,
     /// Current rectangle enclosing the ellipse.
     pub rect: Field<'a, Rectangle>,
+}
+
+impl<'a> Transform2d<'a> for EllipseContext<'a> {
+    #[inline(always)]
+    fn trans(&'a self, x: f64, y: f64) -> EllipseContext<'a> {
+        EllipseContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let trans = translate(x, y);
+                Value(multiply(&trans, self.transform.get()))
+            },
+            rect: Borrowed(self.rect.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn rot_rad(&'a self, angle: f64) -> EllipseContext<'a> {
+        EllipseContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let rot = rotate_radians(angle);
+                Value(multiply(&rot, self.transform.get()))
+            },
+            rect: Borrowed(self.rect.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn scale(&'a self, sx: f64, sy: f64) -> EllipseContext<'a> {
+        EllipseContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let scale = scale(sx, sy);
+                Value(multiply(&scale, self.transform.get()))
+            },
+            rect: Borrowed(self.rect.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn shear(&'a self, sx: f64, sy: f64) -> EllipseContext<'a> {
+        EllipseContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let shear = shear(sx, sy);
+                Value(multiply(&shear, self.transform.get()))
+            },
+            rect: Borrowed(self.rect.get()),
+        }
+    }
 }
 
