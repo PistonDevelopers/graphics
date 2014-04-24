0486fcf7fa94c161a6a75f64a7539619a2516afd
Sven Nilsen <bvssvni@gmail.com>
2014-04-24 10:30:52 +0200

    Added square_centered

--- src/add_rectangle.rs
+++ src/add_rectangle.rs
@@ -9,4 +9,13 @@ pub trait AddRectangle<'a, T> {
     fn square(&'a self, x: f64, y: f64, w: f64) -> T {
         self.rect(x, y, w, w)
     }
+
+    /// Adds a square with coordinates in the center.
+    #[inline(always)]
+    fn square_centered(&'a self, center_x: f64, center_y: f64, radius: f64) -> T {
+        self.rect(center_x - radius, 
+                  center_y - radius, 
+                  2.0 * radius, 
+                  2.0 * radius)
+    }
 }
