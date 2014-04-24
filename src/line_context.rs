53c329667996a3a6ed67ad84c48e1a74c8178357
Sven Nilsen <bvssvni@gmail.com>
2014-04-24 18:33:48 +0200

    Implemented Transform2d for LineContext

--- src/line_context.rs
+++ src/line_context.rs
@@ -1,5 +1,7 @@
 
-use {Field, Matrix2d, Line};
+use {Field, Value, Borrowed, Matrix2d, Line};
+use {Transform2d};
+use vecmath::{translate, rotate_radians, scale, shear, multiply};
 
 /// A line context.
 pub struct LineContext<'a> {
@@ -11,3 +13,52 @@ pub struct LineContext<'a> {
     pub line: Field<'a, Line>,
 }
 
+impl<'a> Transform2d<'a> for LineContext<'a> {
+    #[inline(always)]
+    fn trans(&'a self, x: f64, y: f64) -> LineContext<'a> {
+        LineContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let trans = translate(x, y);
+                Value(multiply(&trans, self.transform.get()))
+            },
+            line: Borrowed(self.line.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn rot_rad(&'a self, angle: f64) -> LineContext<'a> {
+        LineContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let rot = rotate_radians(angle);
+                Value(multiply(&rot, self.transform.get()))
+            },
+            line: Borrowed(self.line.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn scale(&'a self, sx: f64, sy: f64) -> LineContext<'a> {
+        LineContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let scale = scale(sx, sy);
+                Value(multiply(&scale, self.transform.get()))
+            },
+            line: Borrowed(self.line.get()),
+        }
+    }
+
+    #[inline(alwyas)]
+    fn shear(&'a self, sx: f64, sy: f64) -> LineContext<'a> {
+        LineContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let shear = shear(sx, sy);
+                Value(multiply(&shear, self.transform.get()))
+            },
+            line: Borrowed(self.line.get()),
+        }
+    } 
+}
