3bf0c277aaa4f7c056362356836e54e348437b00
Sven Nilsen <bvssvni@gmail.com>
2014-04-25 18:44:03 +0200

    Implemented Fill for PolygonColorContext

--- src/polygon_color_context.rs
+++ src/polygon_color_context.rs
@@ -1,6 +1,7 @@
 use {Field, Value, Borrowed, Matrix2d, Color};
 use vecmath::{multiply, translate, rotate_radians, scale, shear};
-use {Transform2d};
+use {Transform2d, Fill, BackEnd};
+use triangulation::{with_polygon_tri_list_xy_rgba_f32};
 
 /// A polygon color context.
 pub struct PolygonColorContext<'a> {
@@ -64,6 +65,32 @@ impl<'a> Transform2d<'a> for PolygonColorContext<'a> {
             },
             polygon: Borrowed(self.polygon.get()),
             color: Borrowed(self.color.get()),
+        }
+    }
+}
+
+impl<'a> Fill<'a> for PolygonColorContext<'a> {
+    fn fill<B: BackEnd>(&'a self, back_end: &mut B) {
+        if back_end.supports_tri_list_xy_rgba_f32() {
+            let polygon = self.polygon.get();
+            let color = self.color.get();
+            let color: [f32, ..4] = [color[0], color[1], color[2], color[3]];
+            // Complete transparency does not need to be rendered.
+            if color[3] == 0.0 { return; }
+            // Turn on alpha blending if not completely opaque.
+            let needs_alpha = color[3] != 1.0;
+            if needs_alpha { back_end.enable_alpha_blend(); }
+            with_polygon_tri_list_xy_rgba_f32(
+                self.transform.get(),
+                *polygon,
+                color,
+                |vertices, colors| {
+                    back_end.tri_list_xy_rgba_f32(vertices, colors)
+                }
+            );
+            if needs_alpha { back_end.disable_alpha_blend(); }
+        } else {
+            unimplemented!();
         }
     }
 }
