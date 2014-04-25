493416a0de944b2a64c1c9875b3dbfa9cb1a4d4a
Sven Nilsen <bvssvni@gmail.com>
2014-04-25 15:07:22 +0200

    Implemented Transform2d for PolygonContext

--- src/polygon_context.rs
+++ src/polygon_context.rs
@@ -1,4 +1,6 @@
-use {Matrix2d, Field};
+use {Matrix2d, Field, Value, Borrowed};
+use vecmath::{multiply, translate, rotate_radians, scale, shear};
+use {Transform2d};
 
 /// A polygon context.
 pub struct PolygonContext<'a> {
@@ -10,4 +12,52 @@ pub struct PolygonContext<'a> {
     polygon: Field<'a, &'a [f64]>
 }
 
+impl<'a> Transform2d<'a> for PolygonContext<'a> {
+    #[inline(always)]
+    fn trans(&'a self, x: f64, y: f64) -> PolygonContext<'a> {
+        PolygonContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let trans = translate(x, y);
+                Value(multiply(&trans, self.transform.get()))
+            },
+            polygon: Borrowed(self.polygon.get()),
+        }
+    }
 
+    #[inline(always)]
+    fn rot_rad(&'a self, angle: f64) -> PolygonContext<'a> {
+        PolygonContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let rot = rotate_radians(angle);
+                Value(multiply(&rot, self.transform.get()))
+            },
+            polygon: Borrowed(self.polygon.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn scale(&'a self, sx: f64, sy: f64) -> PolygonContext<'a> {
+        PolygonContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let scale = scale(sx, sy);
+                Value(multiply(&scale, self.transform.get()))
+            },
+            polygon: Borrowed(self.polygon.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn shear(&'a self, sx: f64, sy: f64) -> PolygonContext<'a> {
+        PolygonContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let shear = shear(sx, sy);
+                Value(multiply(&shear, self.transform.get()))
+            },
+            polygon: Borrowed(self.polygon.get()),
+        }
+    }
+}
