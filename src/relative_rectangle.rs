0ef556c8b7a625d5ebb2d5fe670fe08e6644823f
Sven Nilsen <bvssvni@gmail.com>
2014-04-26 17:56:24 +0200

    Added `expand`

--- src/relative_rectangle.rs
+++ src/relative_rectangle.rs
@@ -4,6 +4,12 @@ pub trait RelativeRectangle<'a> {
     /// Shrinks the current rectangle equally by all sides.
     fn margin(&'a self, m: f64) -> Self;
 
+    /// Expands the current rectangle equally by all sides.
+    #[inline(always)]
+    fn expand(&'a self, m: f64) -> Self {
+        self.margin(-m)
+    }
+
     /// Moves to a relative rectangle using the current rectangle as tile.
     fn rel(&'a self, x: f64, y: f64) -> Self;
 }
