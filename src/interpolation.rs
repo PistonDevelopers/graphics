44a0ac5e20d51ae429f4e4d90a5ad7d0eb785fa2
Sven Nilsen <bvssvni@gmail.com>
2014-04-27 21:07:19 +0200

    Added `lerp_x` for x = 2, 3, 4, 5

--- src/interpolation.rs
+++ src/interpolation.rs
@@ -15,6 +15,40 @@ pub fn lerp<T: Add<T, T> + Sub<T, T> + Mul<U, T>, U>(a: &T, b: &T, t: &U) -> T {
     *a + (*b - *a) * *t
 }
 
+/// Performs linear interpolation on array of size 3.
+#[inline(always)]
+pub fn lerp_2<T: Add<T, T> + Sub<T, T> + Mul<U, T>, U>(a: &[T, ..2], b: &[T, ..2], t: &U) -> [T, ..2] {
+    [lerp(&a[0], &b[0], t),
+    lerp(&a[1], &b[1], t)]
+}
+
+/// Performs linear interpolation on array of size 3.
+#[inline(always)]
+pub fn lerp_3<T: Add<T, T> + Sub<T, T> + Mul<U, T>, U>(a: &[T, ..3], b: &[T, ..3], t: &U) -> [T, ..3] {
+    [lerp(&a[0], &b[0], t),
+    lerp(&a[1], &b[1], t),
+    lerp(&a[2], &b[2], t)]
+}
+
+/// Performs linear interpolation on array of size 4.
+#[inline(always)]
+pub fn lerp_4<T: Add<T, T> + Sub<T, T> + Mul<U, T>, U>(a: &[T, ..4], b: &[T, ..4], t: &U) -> [T, ..4] {
+    [lerp(&a[0], &b[0], t),
+    lerp(&a[1], &b[1], t),
+    lerp(&a[2], &b[2], t),
+    lerp(&a[3], &b[3], t)]
+}
+
+/// Performs linear interpolation on array of size 5.
+#[inline(always)]
+pub fn lerp_5<T: Add<T, T> + Sub<T, T> + Mul<U, T>, U>(a: &[T, ..5], b: &[T, ..5], t: &U) -> [T, ..5] {
+    [lerp(&a[0], &b[0], t),
+    lerp(&a[1], &b[1], t),
+    lerp(&a[2], &b[2], t),
+    lerp(&a[3], &b[3], t),
+    lerp(&a[4], &b[4], t)]
+}
+
 /// Performs quadratic beziér interpolation.
 /// This is done by nesting linear interpolations.
 /// For more information, see:
