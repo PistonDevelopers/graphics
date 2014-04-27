e3ddda43c3e05b979e4c9cb0db582b014afd89ae
Sven Nilsen <bvssvni@gmail.com>
2014-04-27 14:16:46 +0200

    Implemented `AddPolygons` for `TweenPolygonsColorContext`

--- src/tween_color_context.rs
+++ src/tween_color_context.rs
@@ -1,4 +1,6 @@
-use {Field, Color, Matrix2d};
+use {Field, Borrowed, Value, Color, Matrix2d};
+use {AddPolygons};
+use {TweenPolygonsColorContext};
 
 /// An animation inbetweening context with color.
 pub struct TweenColorContext<'a> {
@@ -10,5 +12,18 @@ pub struct TweenColorContext<'a> {
     pub color: Field<'a, Color>,
     /// Animation inbetweening factor.
     pub tween_factor: Field<'a, f64>,
+}
+
+impl<'a> AddPolygons<'a, TweenPolygonsColorContext<'a>> for TweenColorContext<'a> {
+    #[inline(always)]
+    fn polygons(&'a self, polygons: &'a [&'a [f64]]) -> TweenPolygonsColorContext<'a> {
+        TweenPolygonsColorContext {
+            base: Borrowed(self.base.get()),
+            transform: Borrowed(self.transform.get()),
+            color: Borrowed(self.color.get()),
+            tween_factor: Borrowed(self.tween_factor.get()),
+            polygons: Value(polygons),
+        }
+    }
 }
 
