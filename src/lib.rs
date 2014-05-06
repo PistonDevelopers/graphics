a3e6ccd40a528c0f45c276ac6e5e3e1c4cb165c6
Sven Nilsen <bvssvni@gmail.com>
2014-05-06 13:05:36 +0200

    Added `Image` struct

--- src/lib.rs
+++ src/lib.rs
@@ -154,3 +154,25 @@ impl<'a, T> Field<'a, T> {
     }
 }
 
+/// Represents an image.
+///
+/// Images are often packed together in sprite sheets.  
+/// For this reason it refers to a rectangle within a texture.  
+///
+/// The texture is a unique identifier recognized by the back-end.  
+/// An image contains the size of a texture to be able to  
+/// compute normalized coordinates.  
+///
+/// There is no garbage collection of textures,  
+/// this responsibility is given to the back-end.  
+pub struct Image {
+    /// A unique identifier of the texture, recognizable by back-end.
+    pub texture_id: uint,
+    /// The pixel width of the texture.
+    pub texture_width: u32,
+    /// The pixel height of the texture.
+    pub texture_height: u32,
+    /// The source rectangle in the texture.
+    pub source_rect: PixelRectangle,
+}
+
