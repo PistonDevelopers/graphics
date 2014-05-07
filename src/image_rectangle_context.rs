6cc131e5e6d7a741af2d391690a00897a260bf80
Sven Nilsen <bvssvni@gmail.com>
2014-05-07 13:17:35 +0200

    Added `ImageRectangleContext`

---
+++ src/image_rectangle_context.rs
@@ -0,0 +1,185 @@
+use {
+    BackEnd,
+    Borrowed,
+    Field,
+    Fill,
+    Image,
+    Matrix2d,
+    Rectangle,
+    RelativeRectangle,
+    Value,
+    Transform2d,
+};
+use vecmath::{
+    margin_rectangle,
+    multiply,
+    relative_rectangle,
+    rotate_radians,
+    scale,
+    shear,
+    translate,
+};
+
+/// An image rectangle context.
+pub struct ImageRectangleContext<'a> {
+    /// Base/original transformation.
+    pub base: Field<'a, Matrix2d>,
+    /// Current transformation.
+    pub transform: Field<'a, Matrix2d>,
+    /// Current rectangle.
+    pub rect: Field<'a, Rectangle>,
+    /// Current color.
+    pub image: Field<'a, Image>,
+}
+
+impl<'a> Transform2d<'a> for ImageRectangleContext<'a> {
+    #[inline(always)]
+    fn trans(&'a self, x: f64, y: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let trans = translate(x, y);
+                Value(multiply(&trans, self.transform.get()))
+            },
+            rect: Borrowed(self.rect.get()),
+            image: Borrowed(self.image.get()),
+        }
+    }
+    
+    #[inline(always)]
+    fn trans_local(&'a self, x: f64, y: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let trans = translate(x, y);
+                Value(multiply(self.transform.get(), &trans))
+            },
+            rect: Borrowed(self.rect.get()),
+            image: Borrowed(self.image.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn rot_rad(&'a self, angle: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let rot = rotate_radians(angle);
+                Value(multiply(&rot, self.transform.get()))
+            },
+            rect: Borrowed(self.rect.get()),
+            image: Borrowed(self.image.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn rot_rad_local(&'a self, angle: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let rot = rotate_radians(angle);
+                Value(multiply(self.transform.get(), &rot))
+            },
+            rect: Borrowed(self.rect.get()),
+            image: Borrowed(self.image.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn scale(&'a self, sx: f64, sy: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let scale = scale(sx, sy);
+                Value(multiply(&scale, self.transform.get()))
+            },
+            rect: Borrowed(self.rect.get()),
+            image: Borrowed(self.image.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn scale_local(&'a self, sx: f64, sy: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let scale = scale(sx, sy);
+                Value(multiply(self.transform.get(), &scale))
+            },
+            rect: Borrowed(self.rect.get()),
+            image: Borrowed(self.image.get()),
+        }
+    }
+
+    #[inline(always)]
+    fn shear(&'a self, sx: f64, sy: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let shear = shear(sx, sy);
+                Value(multiply(&shear, self.transform.get()))
+            },
+            rect: Borrowed(self.rect.get()),
+            image: Borrowed(self.image.get()),
+        }
+    }
+    
+    #[inline(always)]
+    fn shear_local(&'a self, sx: f64, sy: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: {
+                let shear = shear(sx, sy);
+                Value(multiply(self.transform.get(), &shear))
+            },
+            rect: Borrowed(self.rect.get()),
+            image: Borrowed(self.image.get()),
+        }
+    }
+}
+
+impl<'a> Fill<'a> for ImageRectangleContext<'a> {
+    fn fill<B: BackEnd>(&'a self, back_end: &mut B) {
+        if back_end.supports_tri_list_xy_f32_rgba_f32_uv_f32() {
+            /*
+            let rect = self.rect.get();
+            let color = self.color.get();
+            let color: [f32, ..4] = [color[0], color[1], color[2], color[3]];
+            // Complete transparency does not need to be rendered.
+            if color[3] == 0.0 { return; }
+            // Turn on alpha blending if not completely opaque.
+            let needs_alpha = color[3] != 1.0;
+            if needs_alpha { back_end.enable_alpha_blend(); }
+            back_end.tri_list_xy_f32_rgba_f32(
+                rect_tri_list_xy_f32(self.transform.get(), rect),
+                rect_tri_list_rgba_f32(color)
+            );
+            if needs_alpha { back_end.disable_alpha_blend(); }
+            */
+        } else {
+            unimplemented!();
+        }
+    }
+}
+
+impl<'a> RelativeRectangle<'a> for ImageRectangleContext<'a> {
+    #[inline(always)]
+    fn margin(&'a self, m: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: Borrowed(self.transform.get()),
+            image: Borrowed(self.image.get()),
+            rect: Value(margin_rectangle(self.rect.get(), m)),
+        }
+    }
+
+    #[inline(always)]
+    fn rel(&'a self, x: f64, y: f64) -> ImageRectangleContext<'a> {
+        ImageRectangleContext {
+            base: Borrowed(self.base.get()),
+            transform: Borrowed(self.transform.get()),
+            image: Borrowed(self.image.get()),
+            rect: Value(relative_rectangle(self.rect.get(), x, y)),
+        }
+    }
+}
