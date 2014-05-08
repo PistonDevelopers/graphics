5e5114f9449c2a9867347150cce7929a334cfc3c
Sven Nilsen <bvssvni@gmail.com>
2014-05-08 13:59:55 +0200

    Added generic parameter to fix `ImageRectangleContext`

--- src/relative_color.rs
+++ src/relative_color.rs
@@ -5,15 +5,15 @@ use {
 };
 
 /// Implemented by contexts that contains color.
-pub trait RelativeColor<'a> {
+pub trait RelativeColor<'a, T> {
     /// Multiplies with red, green, blue and alpha values.
-    fn mul_rgba(&'a self, r: f32, g: f32, b: f32, a: f32) -> Self;
+    fn mul_rgba(&'a self, r: f32, g: f32, b: f32, a: f32) -> T;
 
     /// Mixes the current color with white.
     ///
     /// 0 is black and 1 is white.
     #[inline(always)]
-    fn tint(&'a self, f: f32) -> Self {
+    fn tint(&'a self, f: f32) -> T {
         self.mul_rgba(f, f, f, 1.0)
     }
 
@@ -21,7 +21,7 @@ pub trait RelativeColor<'a> {
     ///
     /// 0 is white and 1 is black.
     #[inline(always)]
-    fn shade(&'a self, f: f32) -> Self {
+    fn shade(&'a self, f: f32) -> T {
         let f = 1.0 - f;
         self.mul_rgba(f, f, f, 1.0)
     }
@@ -29,10 +29,11 @@ pub trait RelativeColor<'a> {
 
 impl<
     'a,
-    T: HasColor<'a, Color> + CanColor<'a, T, Color>
-> RelativeColor<'a> for T {
+    T: HasColor<'a, Color> + CanColor<'a, U, Color>,
+    U
+> RelativeColor<'a, U> for T {
     #[inline(always)]
-    fn mul_rgba(&'a self, r: f32, g: f32, b: f32, a: f32) -> T {
+    fn mul_rgba(&'a self, r: f32, g: f32, b: f32, a: f32) -> U {
         let color = self.get_color();
         self.color([color[0] * r, color[1] * g, color[2] * b, color[3] * a])
     }
