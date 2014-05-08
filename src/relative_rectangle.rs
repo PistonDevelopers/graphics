3724617b67a364f50296891a5c2e1c36a187c91e
Sven Nilsen <bvssvni@gmail.com>
2014-05-08 15:47:28 +0200

    Added generic parameter to `RelativeRectangle`

--- src/relative_rectangle.rs
+++ src/relative_rectangle.rs
@@ -9,55 +9,56 @@ use vecmath::{
 };
 
 /// Should be implemented by contexts that have rectangle information.
-pub trait RelativeRectangle<'a> {
+pub trait RelativeRectangle<'a, T> {
     /// Shrinks the current rectangle equally by all sides.
-    fn margin(&'a self, m: f64) -> Self;
+    fn margin(&'a self, m: f64) -> T;
 
     /// Expands the current rectangle equally by all sides.
     #[inline(always)]
-    fn expand(&'a self, m: f64) -> Self {
+    fn expand(&'a self, m: f64) -> T {
         self.margin(-m)
     }
 
     /// Moves to a relative rectangle using the current rectangle as tile.
-    fn rel(&'a self, x: f64, y: f64) -> Self;
+    fn rel(&'a self, x: f64, y: f64) -> T;
 
     /// Moves to the rectangle above using the current rectangle as tile.
     #[inline(always)]
-    fn up(&'a self) -> Self {
+    fn up(&'a self) -> T {
         self.rel(0.0, 1.0)
     }
 
     /// Moves to the left rectangle using the current rectangle as tile.
     #[inline(always)]
-    fn left(&'a self) -> Self {
+    fn left(&'a self) -> T {
         self.rel(-1.0, 0.0)
     }
 
     /// Moves to the right rectangle using the current rectangle as tile.
     #[inline(always)]
-    fn right(&'a self) -> Self {
+    fn right(&'a self) -> T {
         self.rel(1.0, 0.0)
     }
 
     /// Moves to the rectangle below using the current rectangle as tile.
     #[inline(always)]
-    fn down(&'a self) -> Self {
+    fn down(&'a self) -> T {
         self.rel(0.0, -1.0)
     }
 }
 
 impl<
     'a,
-    T: HasRectangle<'a, Rectangle> + CanRectangle<'a, T, Rectangle>
-> RelativeRectangle<'a> for T {
+    T: HasRectangle<'a, Rectangle> + CanRectangle<'a, U, Rectangle>,
+    U
+> RelativeRectangle<'a, U> for T {
     #[inline(always)]
-    fn margin(&'a self, m: f64) -> T {
+    fn margin(&'a self, m: f64) -> U {
         self.rectangle(margin_rectangle(self.get_rectangle(), m))
     }
 
     #[inline(always)]
-    fn rel(&'a self, x: f64, y: f64) -> T {
+    fn rel(&'a self, x: f64, y: f64) -> U {
         self.rectangle(relative_rectangle(self.get_rectangle(), x, y))
     }
 }
