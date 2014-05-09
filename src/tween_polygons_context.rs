0f42b2a89d8c76d51733e3bc03fd83fced2f00b2
Sven Nilsen <bvssvni@gmail.com>
2014-05-09 09:19:14 +0200

    Impelemented `AddColor` for `TweenPolygonsContext`

--- src/tween_polygons_context.rs
+++ src/tween_polygons_context.rs
@@ -1,7 +1,11 @@
 
 use {
+    AddColor,
+    Borrowed,
     Field,
     Matrix2d,
+    TweenPolygonsColorContext,
+    Value,
 };
 
 /// An animation inbetweening context with color.
@@ -14,5 +18,19 @@ pub struct TweenPolygonsContext<'a> {
     pub tween_factor: Field<'a, f64>,
     /// The animated polygons.
     pub polygons: Field<'a, &'a [&'a [f64]]>,
+}
+
+impl<'a> AddColor<'a, TweenPolygonsColorContext<'a>> for TweenPolygonsContext<'a> {
+    /// Creates a RectangleColorContext.
+    #[inline(always)]
+    fn rgba(&'a self, r: f32, g: f32, b: f32, a: f32) -> TweenPolygonsColorContext<'a> {
+        TweenPolygonsColorContext {
+            base: Borrowed(self.base.get()),
+            transform: Borrowed(self.transform.get()),
+            color: Value([r, g, b, a]),
+            tween_factor: Borrowed(self.tween_factor.get()),
+            polygons: Borrowed(self.polygons.get()),
+        }
+    }
 }
 
