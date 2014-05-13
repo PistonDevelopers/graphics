e5f87473ce6f89d2c9c172072a10b361665841b8
Sven Nilsen <bvssvni@gmail.com>
2014-05-13 13:19:16 +0200

    Fixed doc comment on `round_border_radius`

--- src/add_round_border.rs
+++ src/add_round_border.rs
@@ -1,7 +1,7 @@
 
 /// Implemented by contexts that can add round border.
 pub trait AddRoundBorder<'a, T> {
-    /// Adds a round radius and a width.
+    /// Adds a round border radius.
     fn round_border_radius(&'a self, radius: f64) -> T;
 
     /// Adds a round border width.
