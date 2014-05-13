eddfffd3dcf76cd5306450a2801f4cd04413a05e
Sven Nilsen <bvssvni@gmail.com>
2014-05-13 13:16:37 +0200

    Added `round_border_width` to `AddRoundBorder`

--- src/add_round_border.rs
+++ src/add_round_border.rs
@@ -3,5 +3,11 @@
 pub trait AddRoundBorder<'a, T> {
     /// Adds a round radius and a width.
     fn round_border_radius(&'a self, radius: f64) -> T;
+
+    /// Adds a round border width.
+    #[inline(always)]
+    fn round_border_width(&'a self, width: f64) -> T {
+        self.round_border_radius(0.5 * width)
+    }
 }
 
