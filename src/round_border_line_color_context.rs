fcdf6a6bb2028a049b1fc099ef79e7d3ab8656fe
Sven Nilsen <bvssvni@gmail.com>
2014-05-14 10:58:04 +0200

    Implemented `Stroke` for `RoundBorderLineColorContext`

--- src/round_border_line_color_context.rs
+++ src/round_border_line_color_context.rs
@@ -1,8 +1,13 @@
 use {
+    BackEnd,
     Color,
     Field,
     Line,
     Matrix2d,
+    Stroke,
+};
+use triangulation::{
+    with_round_border_line_tri_list_xy_f32_rgba_f32
 };
 
 /// A line context with round border information.
@@ -19,3 +24,30 @@ pub struct RoundBorderLineColorContext<'a> {
     pub round_border_radius: Field<'a, f64>,
 }
 
+impl<'a> Stroke<'a> for RoundBorderLineColorContext<'a> {
+    fn stroke<B: BackEnd>(&'a self, back_end: &mut B) {
+        if back_end.supports_tri_list_xy_f32_rgba_f32() {
+            let line = self.line.get();
+            let round_border_radius = self.round_border_radius.get();
+            let color = self.color.get();
+            // Complete transparency does not need to be rendered. 
+            if color[3] == 0.0 { return; }
+            // Turn on alpha blending if not completely opaque.
+            let needs_alpha = color[3] != 1.0;
+            if needs_alpha { back_end.enable_alpha_blend(); }
+            with_round_border_line_tri_list_xy_f32_rgba_f32(
+                64,
+                self.transform.get(),
+                line,
+                round_border_radius,
+                *color,
+                |vertices, colors| {
+                    back_end.tri_list_xy_f32_rgba_f32(vertices, colors)
+                }
+            );
+            if needs_alpha { back_end.disable_alpha_blend(); }
+        } else {
+            unimplemented!();
+        }
+    }
+}
