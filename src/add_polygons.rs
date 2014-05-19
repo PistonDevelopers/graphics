c427eeb0f67d0b0b7105d2badace57bb853fdb8c
Sven Nilsen <bvssvni@gmail.com>
2014-05-19 12:45:01 +0200

    Using type alias for `Polygons` in `AddPolygons`

--- src/add_polygons.rs
+++ src/add_polygons.rs
@@ -1,7 +1,10 @@
+use {
+    Polygons,
+};
 
 /// Implemented by tweening contexts that can add polygons.
 pub trait AddPolygons<'a, T> {
     /// Add polygons.
-    fn polygons(&'a self, polygons: &'a [&'a [f64]]) -> T;
+    fn polygons(&'a self, polygons: Polygons<'a>) -> T;
 }
 
