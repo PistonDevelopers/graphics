de13463723770485e41b595e82f364b473c366a3
Sven Nilsen <bvssvni@gmail.com>
2014-10-18 12:50:28 +0200

    Reexported `add` and `sub`
    Closes https://github.com/PistonDevelopers/graphics/issues/646

--- src/vecmath.rs
+++ src/vecmath.rs
@@ -5,6 +5,8 @@ use vecmath_lib;
 
 pub use vecmath_lib::row_mat2x3_mul as multiply;
 pub use vecmath_lib::vec2_dot as dot;
+pub use vecmath_lib::vec2_add as add;
+pub use vecmath_lib::vec2_sub as sub;
 
 use internal::{
     Area,
