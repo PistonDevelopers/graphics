98828132d328d5e7281568594d09e4b6348ec132
Sven Nilsen <bvssvni@gmail.com>
2014-11-30 00:40:08 +0100

    Added color to Image

--- src/image.rs
+++ src/image.rs
@@ -8,6 +8,8 @@ use ImageSize;
 pub struct Image<'a, I: 'a + ImageSize> {
     /// The texture to draw with
     pub texture: &'a I,
+    /// The color
+    pub color: internal::Color,
     /// The image source rectangle
     pub source_rectangle: internal::SourceRectangle,
 }
@@ -17,7 +19,7 @@ impl<'a, I: ImageSize> Image<'a, I> {
     pub fn draw<B: BackEnd<I>>(&self, c: &Context, back_end: &mut B) {
         use internal::Scalar;
 
-        let color: [f32, ..4] = [1.0, 1.0, 1.0, 1.0];
+        if self.color[3] == 0.0 { return; }
         let rect = [
             0.0,
             0.0,
@@ -25,9 +27,8 @@ impl<'a, I: ImageSize> Image<'a, I> {
             self.source_rectangle[3] as Scalar
         ];
         // Complete transparency does not need to be rendered.
-        if color[3] == 0.0 { return; }
         back_end.enable_texture(self.texture);
-        back_end.color(color);
+        back_end.color(self.color);
         back_end.tri_list_uv(
             &triangulation::rect_tri_list_xy(c.transform, rect),
             &triangulation::rect_tri_list_uv(self.texture, self.source_rectangle)
