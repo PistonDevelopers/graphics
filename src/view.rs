0eeca96673ee48f92af6cafce39bf3f0aa184929
Sven Nilsen <bvssvni@gmail.com>
2014-11-29 22:45:29 +0100

    Moved `View` impl methods to trait
    Closes https://github.com/PistonDevelopers/graphics/issues/715

--- src/view.rs
+++ src/view.rs
@@ -15,26 +15,42 @@ use vecmath::{
 
 /// Should be implemented by contexts that
 /// draws something relative to view.
-pub trait View {
+pub trait View:
+    HasViewTransform + CanViewTransform
+  + HasTransform + CanTransform
+{
     /// Moves the current transform to the view coordinate system.
     ///
     /// This is usually [0.0, 0.0] in the upper left corner
     /// with the x axis pointing to the right
     /// and the y axis pointing down.
-    fn view(&self) -> Self;
+    #[inline(always)]
+    fn view(&self) -> Self {
+        self.transform(self.get_view_transform())
+    }
 
     /// Moves the current transform to the default coordinate system.
     ///
     /// This is usually [0.0, 0.0] in the center
     /// with the x axis pointing to the right
     /// and the y axis pointing up.
-    fn reset(&self) -> Self;
+    #[inline(always)]
+    fn reset(&self) -> Self {
+        self.transform(identity())
+    }
 
     /// Stores the current transform as new view.
-    fn store_view(&self) -> Self;
+    #[inline(always)]
+    fn store_view(&self) -> Self {
+        self.view_transform(self.get_transform())
+    }
 
     /// Computes the current view size.
-    fn get_view_size(&self) -> (Scalar, Scalar);
+    #[inline(always)]
+    fn get_view_size(&self) -> (Scalar, Scalar) {
+        let scale = get_scale(self.get_view_transform());
+        (2.0 / scale[0], 2.0 / scale[1])
+    }
 }
 
 impl<
@@ -42,25 +58,5 @@ impl<
      + HasTransform
      + CanViewTransform
      + CanTransform
-> View for T {
-    #[inline(always)]
-    fn view(&self) -> T {
-        self.transform(self.get_view_transform())
-    }
+> View for T {}
 
-    #[inline(always)]
-    fn reset(&self) -> T {
-        self.transform(identity())
-    }
-
-    #[inline(always)]
-    fn store_view(&self) -> T {
-        self.view_transform(self.get_transform())
-    }
-
-    #[inline(always)]
-    fn get_view_size(&self) -> (Scalar, Scalar) {
-        let scale = get_scale(self.get_view_transform());
-        (2.0 / scale[0], 2.0 / scale[1])
-    }
-}
